1da6df52507af13f6b03e03a040b83c1ace03504
Aniket06062002 <75536999+Aniket06062002@users.noreply.github.com>
2021-09-16 20:28:48 +0530

    Add project files.

---
+++ Array Problems/ArrayRotation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problems
+{
+    public class ArrayRotation
+    {
+        public static void Main()
+        {
+            int[] arr = new int[] {1,2,3,4,5,6,7 };
+            int d = 2;
+            leftRotate(arr, arr.Length, d);
+            printArray(arr, arr.Length);
+            Console.ReadLine();
+        }
+
+        private static void printArray(int[] arr, int length)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + ",");
+            }
+        }
+
+        private static void leftRotate(int[] arr, int length, int d)
+        {
+            for (int i = 0; i <d; i++)
+            {
+                oneRotate(arr, length);
+            }
+        }
+
+        private static void oneRotate(int[] arr, int length)
+        {
+            int temp = arr[0];
+            for (int i = 0; i < length-1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            arr[length - 1] = temp;
+        }
+    }
+}
---
+++ Array Problems/Reversal_Algorithm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problems
+{
+    public class Reversal_Algorithm
+    {
+        public void leftRotate(int[] arr,int d)
+        {
+            if (d==0)
+            {
+                return;
+            }
+            int n = arr.Length;
+
+            rotateArry(arr, 0, d-1);
+            rotateArry(arr, d, n - 1);
+            rotateArry(arr, 0, n-1);
+        }
+
+        public void rotateArry(int[] arr, int start, int end)
+        {
+            
+            while (start<end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+
+        public static void Main()
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int d = 2;
+            Reversal_Algorithm reversal_Algorithm = new Reversal_Algorithm();
+            reversal_Algorithm.leftRotate(arr, d);
+            reversal_Algorithm.printArray(arr);
+            Console.ReadLine();
+        }
+
+        private void printArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + ",");
+            }
+        }
+    }
+}
